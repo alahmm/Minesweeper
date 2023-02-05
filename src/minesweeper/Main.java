857cf8890eee5d256cf1ce3eadbae18fd133c9ce
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-02-05 23:46:26 +0100

    calculating the number of mines around a cell and put it in that cell

--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -5,6 +5,169 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    public static char[][] minesaround(char[][] matrix) {
+        try {
+            int counterX = 0;
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (matrix[i][j] == '.') {
+                        /**
+                         * in the corner
+                         */
+                        if (i == 0 && j == 0) {
+                            if (matrix[i + 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                        } else if (i == 0 && j == 8) {
+                            if (matrix[i + 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+
+                        } else if (i == 8 && j == 0) {
+                            if (matrix[i - 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                        } else if (i == 8 && j == 8) {
+                            if (matrix[i - 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                        }
+                        /**
+                         * on the side
+                         */
+                        else if (j == 0) {
+                            if (matrix[i + 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                        } else if (j == 8) {
+                            if (matrix[i + 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                        } else if (i == 0) {
+                            if (matrix[i + 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                        } else if (i == 8) {
+                            if (matrix[i - 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                        }
+                        /**
+                         * middel
+                         */
+                        else {
+                            if (matrix[i - 1][j] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i - 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j + 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j - 1] == 'X') {
+                                counterX ++;
+                            }
+                            if (matrix[i + 1][j] == 'X') {
+                                counterX ++;
+                            }
+                        }
+
+                    }
+                    if (counterX != 0) {
+                        char var = (char) (counterX + 48);
+                        matrix[i][j] = var;
+                        counterX = 0;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return matrix;
+    }
     public static char[][] minesprovider(char[][] matrix, int number) {
         int counterX =0;
         Random random = new Random();
@@ -49,7 +212,7 @@ public class Main {
         }
         Scanner scanner = new Scanner(System.in);
         int numberOfMines = scanner.nextInt();
-        printer(minesprovider(matrixOfMines, numberOfMines));
+        printer(minesaround(minesprovider(matrixOfMines, numberOfMines)));
 
     }
 }
